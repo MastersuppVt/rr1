ea3640d451cb3240e9e9178d430d378a65a4bfbc
MastersuppVt <animastercompany@gmail.com>
2022-02-01 00:38:24 -0300

    r5

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, { Component, useState, useEffect } from "react";
+
 const Icount = () => {
     const [counter, setCounter] = useState(1);
     const [stock, setStock] = useState(1);
     const [producto, setProducto] = useState(1);
+    useEffect(() => {
+        itemCount(Number(prompt("stock")), Number(prompt("inicial")))
+        return () => {
+            console.log("hi")
+        };
+    }, []);
+
     function itemCount(stock, inicial, onAdd) {
-        var n1 = Number(prompt("stock"))
-        var n2 = Number(prompt("inicial"))
+        var n1 = stock;
+        var n2 = inicial;
         setStock(n1)
         setProducto(n2)
     }
     const onAdd = () => {
-        
+
         if (producto <= stock) {
             setProducto(producto + counter)
             setStock(stock - counter)
             setCounter(0);
         }
     }
-
     const quitar = () => {
-        if (producto > stock) {
+        if (producto >0&&(producto - counter)>=0) {
             setProducto(producto - counter)
             setStock(stock + counter)
             setCounter(0);
@@ -48,7 +55,7 @@ const Icount = () => {
             <p>producto: {producto}</p>
             <p>stock: {stock}</p>
             <div className='btn-section'>
-            <button onClick={itemCount}>add</button>
+
                 <button onClick={onAdd}>Añadir</button>
                 <button onClick={quitar}>quitar</button>
                 <button onClick={handlerCounterUp}>Incrementar</button>
